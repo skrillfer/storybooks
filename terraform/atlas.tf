fecb4e26ccfa09924e9f5da111034224bfc8fc29
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2021-12-31 01:16:34 -0600

    mongo_cluster, mongo_user, and mongodbatlas_project_ip_access_list configuration

--- terraform/atlas.tf
+++ terraform/atlas.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source = "terraform-providers/mongodbatlas"
-      version = "= 0.8.0"
+      #version = "= 0.6.0"
     }
   }
 }
@@ -13,5 +13,42 @@ provider "mongodbatlas" {
 }
 
 # Cluster
+resource "mongodbatlas_cluster" "mongo_cluster" {
+  project_id   = var.atlas_project_id
+  name         = "${var.app_name}-${terraform.workspace}"
+  cluster_type = "REPLICASET"
+  replication_specs {
+    num_shards = 1
+    regions_config {
+      region_name     = "CENTRAL_US"
+      electable_nodes = 3
+      priority        = 7
+      read_only_nodes = 0
+    }
+  }
+  provider_backup_enabled                 = true
+  auto_scaling_disk_gb_enabled = true
+  mongo_db_major_version       = "4.2"
+
+  # Provider Settings "block"
+  provider_name               = "GCP"
+  disk_size_gb                = 10
+  provider_instance_size_name = "M10"
+}
 # DB User
+resource "mongodbatlas_database_user" "mongo_user" {
+  username           = "storybooks-user-${terraform.workspace}"
+  password           = var.atlas_user_password
+  project_id         = var.atlas_project_id
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "readWrite"
+    database_name = "storybooks"
+  }
+}
 # IP Whitelist
+resource "mongodbatlas_project_ip_access_list" "test" {
+  project_id = var.atlas_project_id
+  ip_address = google_compute_address.ip_address.address
+}
