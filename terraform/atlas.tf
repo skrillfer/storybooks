24069a0ddee677ffdc77af43825c1ed5f0159487
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2021-12-30 22:13:43 -0600

    created atlas.tf file

---
+++ terraform/atlas.tf
@@ -0,0 +1,9 @@
+# Configure the MongoDB Atlas Provider
+provider "mongodbatlas" {
+  public_key = var.mongodbatlas_public_key
+  private_key  = var.mongodbatlas_private_key
+}
+
+# Cluster
+# DB User
+# IP Whitelist
