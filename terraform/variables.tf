c8ce481b6abb6ca525991bfa8a5ff4ca7980ce76
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2021-12-30 22:14:34 -0600

    created variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,16 @@ variable "app_name" {
     type = string
     default = ""
 }
+
+### ATLAS
+variable "mongodbatlas_public_key" {
+  type = string
+}
+variable "mongodbatlas_private_key" {
+  type = string
+}
+
+### GCP
+variable "gcp_machine_type" {
+  type = string
+}
